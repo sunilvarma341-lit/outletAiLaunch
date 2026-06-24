3d634638f574becf6622cba797261418dd04c42a
sunilvarma341-lit <sunilvarma@lohiyaai.com>
2026-06-24 19:04:58 +0530

    Set Router basename to use environment variable for base URL

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import './App.css'
 
 function App() {
   return (
-    <Router>
+    <Router basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route path="/" element={<AppLaunch />} />
         <Route path="/config" element={<ConfigPanel />} />
